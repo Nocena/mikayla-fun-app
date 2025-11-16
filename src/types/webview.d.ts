7e30f3c5ada8ddb7471dd4aaaa6efba95f504cd7
matijamicunovic629 <micunovicmatija629@gmail.com>
2025-11-16 12:40:32 +0100

    [Fix] Make webview as real chrome browser

---
+++ src/types/webview.d.ts
@@ -0,0 +1,16 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    webview: React.DetailedHTMLProps<
+      React.HTMLAttributes<WebviewTag> & {
+        src?: string;
+        allowpopups?: boolean;
+        webpreferences?: string;
+        partition?: string;
+        useragent?: string;
+      },
+      WebviewTag
+    >;
+  }
+}
+
+
